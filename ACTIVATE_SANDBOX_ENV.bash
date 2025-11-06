7b32799241231f030e9513d1d492857603f1b1bd
Brian Kearney <briankearney@mcindi.com>
2025-11-06 11:14:17 -0500

    fix a few typos

--- ACTIVATE_SANDBOX_ENV.bash
+++ ACTIVATE_SANDBOX_ENV.bash
@@ -2,8 +2,8 @@
 
 deactivate
 export PYTHON_VENV=~/Documents/Python/ansible &&\
-export ROLE_PATH=~/Documents/GitHub/ans_dev_sandbox_role
-source $PYTHON_VENV/bin/activate &&\
+export ROLE_PATH=~/Documents/GitHub/ans_dev_sandbox_role &&\
+ source $PYTHON_VENV/bin/activate &&\
  cd $ROLE_PATH &&\
  export ANSIBLE_DISPLAY_ARGS_TO_STDOUT=false &&\
  export ANSIBLE_CALLBACKS_ENABLED='yaml,profile_tasks' &&\
@@ -14,6 +14,6 @@ source $PYTHON_VENV/bin/activate &&\
  export ANSIBLE_LIBRARY=library &&\
  export ANSIBLE_STDOUT_CALLBACK=yaml &&\
  export ANSIBLE_VAULT_PASSWORD_FILE=$ROLE_PATH/vault-pw.txt &&\
- export SANDBOX_GITHUB_SSH_KEY=~/.ssh/id_rsa &&\
+ export SANDBOX_GITHUB_SSH_KEY=~/.ssh/id_rsa
 
 alias avdad='python $ROLE_PATH/DECRYPT_VAULTED_ITEMS.py'
