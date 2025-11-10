aebc6ac3bbc76bc598b41d80aa75e4c456f595ad
Brian Kearney <briankearney@mcindi.com>
2025-11-10 17:16:27 -0500

    use hidden virtual environment subdirectory

--- ACTIVATE_SANDBOX_ENV.bash
+++ ACTIVATE_SANDBOX_ENV.bash
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
 
 deactivate
-export PYTHON_VENV=~/Documents/Python/py313_ans219 &&\
 export ROLE_PATH=~/Documents/GitHub/ans_dev_sandbox_role &&\
- source $PYTHON_VENV/bin/activate &&\
  cd $ROLE_PATH &&\
+ source ./.venv/bin/activate &&\
  export ANSIBLE_DISPLAY_ARGS_TO_STDOUT=false &&\
  export ANSIBLE_CALLBACKS_ENABLED='yaml,profile_tasks' &&\
  export ANSIBLE_LOAD_CALLBACK_PLUGINS=true &&\
@@ -14,6 +13,5 @@ export ROLE_PATH=~/Documents/GitHub/ans_dev_sandbox_role &&\
  export ANSIBLE_LIBRARY=library &&\
  export ANSIBLE_STDOUT_CALLBACK=yaml &&\
  export ANSIBLE_VAULT_PASSWORD_FILE=$ROLE_PATH/vault-pw.txt &&\
- export SANDBOX_GITHUB_SSH_KEY=~/.ssh/id_rsa
-
-alias avdad='python $ROLE_PATH/DECRYPT_VAULTED_ITEMS.py'
+ export SANDBOX_GITHUB_SSH_KEY=~/.ssh/id_rsa &&\
+ alias avdad='python $ROLE_PATH/DECRYPT_VAULTED_ITEMS.py'
